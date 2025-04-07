c1c2fb9721cbd784f548889d4f9310c1cb1b1466
Austin <20157814+adurr21@users.noreply.github.com>
2025-04-07 17:25:52 -0400

    Updated homepage to add contact component

--- dawghouse/app/page.tsx
+++ dawghouse/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
-import Navbar from './components/navbar';
+import Navbar from '@/app/components/navbar';
 import About from '@/app/components/about';
+import Contact from "@/app/components/contact";
 
 export default function Home() {
   return (
+    <>
     <About />
+    <Contact />
+    </>
   );
 }
