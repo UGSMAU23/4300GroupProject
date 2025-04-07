636b60d66493fdc00a6210adf21d442ee564f11a
Austin <20157814+adurr21@users.noreply.github.com>
2025-04-07 17:25:12 -0400

    Fixed component background color width

--- dawghouse/app/components/about.tsx
+++ dawghouse/app/components/about.tsx
@@ -10,7 +10,7 @@ const DawgHouse = () => {
                 <h1 className="text-red-600">House</h1>
                 <h1 className="text-black">?</h1>
             </div>
-            <div className="bg-red-200 mt-10">
+            <div className="bg-red-200 mt-10 w-screen">
                 <div className='flex flex-row justify-center items-stretch text-center mt-5 mb-5'>
                     <div className="flex flex-col flex-2 items-center justify-center text-lg pl-5 pr-5">
                         <p>DawgHouse is a free housing network for University of Georgia students, allowing quick and efficient roommate matching.<br /><br /></p>
