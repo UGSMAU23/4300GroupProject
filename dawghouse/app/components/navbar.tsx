0661dcf355b44f1d5e890f75087503c12051dc94
Austin <20157814+adurr21@users.noreply.github.com>
2025-04-09 14:17:56 -0400

    Updated navbar
    Updated the navbar to use next.js Link and Image components instead of <a> and <img> to allow for client-side route navigation.

--- dawghouse/app/components/navbar.tsx
+++ dawghouse/app/components/navbar.tsx
@@ -1,30 +1,31 @@
 // components/Navbar.tsx
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-white sticky top-0 z-10">
       <div className="flex items-center">
-      <a href="#top" className="flex items-center">
-          <img src="dog.svg" alt="DawgHouse Logo" className="w-12 h-12 mr-2" />
+      <Link href="#top" className="flex items-center">
+          <Image src="dog.svg" alt="DawgHouse Logo" width="0" height="0" className="w-12 h-12 mr-2" />
           <div className="flex items-center cursor-pointer">
             <span className="font-bold text-3xl">
               Dawg<span className="text-red-600">House</span>
             </span>
           </div>
-        </a>
+        </Link>
       </div>
       <div className="flex items-center space-x-10">
-        <a href="#about">
+        <Link href="#about">
           <span className="cursor-pointer hover:text-red-600 transition-colors font-bold">About</span>
-        </a>
-        <a href="#contact">
+        </Link>
+        <Link href="#contact">
           <span className="cursor-pointer hover:text-red-600 transition-colors font-bold">Contact</span>
-        </a>
-        <a href="#" className="cursor-pointer hover:text-red-600 transition-colors font-bold">Log In</a>
-        <a href="#">
+        </Link>
+        <Link href="#" className="cursor-pointer hover:text-red-600 transition-colors font-bold">Log In</Link>
+        <Link href="#">
           <button className="bg-red-600 text-white px-4 py-2 rounded cursor-pointer hover:bg-red-700">Get Started</button>
-        </a>
+        </Link>
       </div>
     </nav>
   );
