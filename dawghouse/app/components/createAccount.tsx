0064b4c939d12e76797a34f51c3d80e44349ea17
Austin <20157814+adurr21@users.noreply.github.com>
2025-04-09 22:54:08 -0400

    Updated styling for login and createAccount components

--- dawghouse/app/components/createAccount.tsx
+++ dawghouse/app/components/createAccount.tsx
@@ -1,25 +1,33 @@
 import Form from 'next/form';
+import Link from 'next/link';
+import { League_Spartan } from 'next/font/google';
+
+const league = League_Spartan({
+    weight: '400',
+    subsets: ['latin']
+});
+
 
 const CreateAccount = () => {
     return (
-        <div className="flex justify-center items-center flex-col mt-10 mb-10">
-            <div className="flex text-5xl">
-                <h1 className="text-black">Create Account</h1>
+        <div className="flex justify-start items-center flex-col bg-pink-50 h-screen">
+            <div className="flex text-5xl mt-30">
+                <h1 className={`text-black ${league.className}`}>Create Account</h1>
             </div>
-            <div className="bg-red-200 mt-10 w-screen flex justify-center items-center">
-            <div className="bg-white border-solid rounded-lg mt-10 mb-10 shadow-md w-[500px]">
+            <div className="w-screen flex justify-center items-center">
+            <div className="bg-white border-solid rounded-lg mt-10 mb-10 shadow-lg/50 w-[500px]">
                 <Form action={ "/createAccount" } className="flex flex-col m-10">
                         <label htmlFor="email">Email</label>
-                        <input className="border border-gray-400 rounded-sm px-2 mt-1" id="email" name="email" type="email" placeholder="Email" required/>
+                        <input className="border border-gray-400 rounded-sm px-2 mt-1 shadow-md" id="email" name="email" type="email" placeholder="Email" required/>
                         <label htmlFor="password" className="mt-3">Password</label>
-                        <input className="border border-gray-400 rounded-sm px-2 mt-1" id="password" name="password" type="password" placeholder="Password" required/>
-                        <div className="bg-red-800 rounded-md flex justify-center items-center mt-5">
+                        <input className="border border-gray-400 rounded-sm px-2 mt-1 shadow-md" id="password" name="password" type="password" placeholder="Password" required/>
+                        <div className="bg-red-800 rounded-md flex justify-center items-center mt-5 shadow-md">
                             <input className="text-white" type="submit" value="Login"/>
                         </div>
                     </Form>
                     
                     <div className="text-sm text-left px-12 pb-10">
-                        <a href="..." className="text-black underline block">Already have an account? Log in here.</a>
+                        <Link href="/login" className="text-black underline block">Already have an account? Log in here.</Link>
                     </div>
                 </div>
             </div>
