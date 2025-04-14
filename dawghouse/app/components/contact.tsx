5dba410b8afdd1ad2c5ae93732fecd4e0b0b45f0
jacobsehl <jacobsehl@gmail.com>
2025-04-14 13:11:07 -0400

    Mobile Responsive Contact Section

--- dawghouse/app/components/contact.tsx
+++ dawghouse/app/components/contact.tsx
@@ -3,8 +3,8 @@ import Form from 'next/form'
 
 const Contact = () => {
     return (
-        <div className="flex justify-center items-center flex-col bg-white" >
-            <div className="flex text-5xl mt-10">
+        <div className="flex justify-center items-center flex-col bg-white px-4 md:px-0" >
+            <div className="flex text-3xl md:text-5xl mt-10">
                 <h1 className="text-black">Contact Us</h1>
             </div>
             <div className="flex justify-center items-center">
@@ -15,7 +15,7 @@ const Contact = () => {
                         <label htmlFor="email" className='mt-3' >Email</label>
                         <input className='border-solid border-gray-400 border-1 rounded-lg px-2' id='email' name='email' type='email' required></input>
                         <label htmlFor="message" className='mt-3' >Message</label>
-                        <textarea className='h-25 w-100 border-solid border-gray-400 border-1 rounded-lg px-1 pt-1' id='message' name='message' required></textarea>
+                        <textarea className='h-25 md:w-100 border-solid border-gray-400 border-1 rounded-lg px-1 pt-1' id='message' name='message' required></textarea>
                         <div className='bg-red-800 flex justify-center items-center mt-5 border-solid border-black border-1 cursor-pointer rounded-md'>
                             <input className='text-white' type='submit' id='submit' value='Submit'></input>
                         </div>
