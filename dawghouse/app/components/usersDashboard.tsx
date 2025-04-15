4d51eb1e3206439ac387c3bb17726f65c42022b1
jacobsehl <jacobsehl@gmail.com>
2025-04-15 15:17:42 -0400

    matches page ui fix

--- dawghouse/app/components/usersDashboard.tsx
+++ dawghouse/app/components/usersDashboard.tsx
@@ -73,7 +73,7 @@ const UsersDashboard = () => {
                 <p className="text-red-500 font-semibold">
                 {Math.floor(Math.random() * 21) + 80}% Compatible
                 </p>
-                <button onClick={() => openModal(index)} className="mt-4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">
+                <button onClick={() => openModal(index)} className="mt-4 cursor-pointer bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">
                 Learn More
                 </button>
             </div>
