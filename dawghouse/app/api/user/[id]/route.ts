cc2873ebf09432d264ae6a978aa7c7f95c26e64a
jacobsehl <jacobsehl@gmail.com>
2025-04-14 21:10:20 -0400

    Update userId route
    User can update username email answers or password

--- dawghouse/app/api/user/[id]/route.ts
+++ dawghouse/app/api/user/[id]/route.ts
@@ -2,15 +2,26 @@ import { NextResponse, NextRequest } from "next/server";
 import mongoose from "mongoose";
 import connectMongoDB from "@/config/mongodb";
 import User from "@/models/UserSchema";
+import bcrypt from "bcryptjs";
 
 interface RouteParams {
     params: {id: string};
 }
 
 export async function PUT(request: NextRequest, {params}:RouteParams) {
-    const {id} = await params;
-    const {username: username} = await request.json();
+    const { id } = params;
+    const { username, email, answers, password } = await request.json();
     await connectMongoDB();
-    await User.findByIdAndUpdate(id, {username});
+    const update: any = {};
+
+    if (username) update.username = username;
+    if (email) update.email = email;
+    if (answers) update.answers = answers;
+    if (password) {
+        const hashedPassword = await bcrypt.hash(password, 15);
+        update.password = hashedPassword;
+    }
+    
+    await User.findByIdAndUpdate(id, update);
     return NextResponse.json({message: "User updated"}, {status: 200});
 }
