eae71e7162cee34d338af82cea9b42e33b33ca06
Austin <20157814+adurr21@users.noreply.github.com>
2025-04-14 20:33:08 -0400

    Added answers to UserSchema

--- dawghouse/models/UserSchema.ts
+++ dawghouse/models/UserSchema.ts
@@ -4,12 +4,14 @@ export interface User extends Document {
     username: String;
     email: String;
     password: String;
+    answers: Array<String>;
 }
 
 const UserSchema = new Schema<User>({
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
+    answers: {type: [String]}
 });
 
 const User: Model<User> = mongoose.models.User || mongoose.model<User>("User", UserSchema);
